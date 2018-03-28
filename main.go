778cea5b8c628ed8756d1051088158337307784e
evan <281406674@qq.com>
2018-03-28 12:28:34 +0800

    采用http监听端口的方式卡住而不是tail日志的方式
    Signed-off-by: evan <281406674@qq.com>

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	core "udp-proxy/core"
 	"github.com/astaxie/beego/logs"
-	"os"
 	dao "udp-proxy/mysql"
+	"net/http"
+	"fmt"
 )
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello world!")
+}
 
 func main() {
 	// Forward(src, dst). It's asynchronous.
@@ -20,5 +29,11 @@ func main() {
 	}
 	logs.Info("UDP转发代理初始化成功,正在监听",src)
 	//确保程序不退出
-	os.Stdin.Read(make([]byte,1))
+	//os.Stdin.Read(make([]byte,1))
+	http.HandleFunc("/", helloHandler)
+	logs.Info("Http listening on port 8888")
+	httpError := http.ListenAndServe(":8888", nil)
+	if httpError != nil {
+		logs.Error(httpError)
+	}
 }
